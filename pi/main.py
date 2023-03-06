2cbe05c3ed362e8f118fe1733835e037dd74c983
us3-r <z4nhr1@gmail.com>
2023-03-06 18:59:14 +0100

    video adj

--- pi/main.py
+++ pi/main.py
@@ -8,10 +8,6 @@ ser = serial.Serial('/dev/ttyUSB0', 9600)
 #util.Messages.Terminal.init
 ser.write('Hello from Raspberry Pi!'.encode())
 
-# 1. ui k bo skos przgan da se ne vid pac oz da se ne more exitat (plus ma gumbke za izbiro vrat in pol odpre novo okno za opravljanje vrat),
-# 2. nek fail-safe k se sproz ce se zapre ta program (aka se ne backproces k checka ce ta program deluje al ne (aka bash shit )) -> sproz da se al restarta program al pa sam ugasne,
-# 3. funkcija k bo tejkala care za predvajanje videa (ce se prtisne gumb pr XOR vratih da pol predvaja npr: XOR.mp4 na loopu za tko idk x sekund; pol pa se vrne nazaj na prvortn ui),
-# 4. funkcija k bo komunicirala z arduinotom (če se tm zazna da se ksn gumb prtisne da se to poročin in posledično predvaja simulacija)
 def get_video(select :str):
     import os
     gate_list = [
@@ -24,9 +20,10 @@ def get_video(select :str):
         "sr"
     ]
     path = f"anim/{select}.mp4" if select in gate_list else None
-    print(path)
-    #if path is not None: os.system(f"vlc --play-and-exit {path}")
-
+    #print(path)
+    if path is not None: 
+        os.system(f"vlc --play-and-exit {path}")
+        return True
 Start.init_check()
 
 while True:
@@ -38,4 +35,5 @@ while True:
         log_rep = f"[ at {ct} ]\nSerial data: {ser}\nDecoded message: {message}\n"
         with open("log.log", "a") as file:
             file.write(log_rep)
-        get_video(message)
+        if (get_video(message)):
+            time.sleep(7)
