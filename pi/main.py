03c043cd512665d1cc54ec22d9081a2a9d4ba03e
us3-r <z4nhr1@gmail.com>
2023-03-06 15:19:31 +0100

    time

--- pi/main.py
+++ pi/main.py
@@ -12,9 +12,16 @@ ser.write('Hello from Raspberry Pi!'.encode())
 # 2. nek fail-safe k se sproz ce se zapre ta program (aka se ne backproces k checka ce ta program deluje al ne (aka bash shit )) -> sproz da se al restarta program al pa sam ugasne,
 # 3. funkcija k bo tejkala care za predvajanje videa (ce se prtisne gumb pr XOR vratih da pol predvaja npr: XOR.mp4 na loopu za tko idk x sekund; pol pa se vrne nazaj na prvortn ui),
 # 4. funkcija k bo komunicirala z arduinotom (če se tm zazna da se ksn gumb prtisne da se to poročin in posledično predvaja simulacija)
+def do_smthing(v):
+    if v == 1:
+        print("vrata so bla bla")
+        import time
+        time.sleep(5)
 
 while True:
     if ser.in_waiting > 0:
         message = ser.readline().strip().decode('utf-8')  # read the message from serial
-        print(message)  # print the message to the console
-
+        #print(message)  # print the message to the console
+        import time
+        ct =time.strftime("%H:%M")
+        log_rep = f"[at {ct}]\nSerial data: {ser}\nDecoded message: {message}"
