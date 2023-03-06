ef84b7e51bedb2ff39d3c3229c26f0c8e0a60635
us3-r <z4nhr1@gmail.com>
2023-03-06 15:27:08 +0100

    sxax

--- pi/main.py
+++ pi/main.py
@@ -5,7 +5,7 @@ import util
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-util.Messages.Terminal.init
+#util.Messages.Terminal.init
 ser.write('Hello from Raspberry Pi!'.encode())
 
 # 1. ui k bo skos przgan da se ne vid pac oz da se ne more exitat (plus ma gumbke za izbiro vrat in pol odpre novo okno za opravljanje vrat),
@@ -13,11 +13,12 @@ ser.write('Hello from Raspberry Pi!'.encode())
 # 3. funkcija k bo tejkala care za predvajanje videa (ce se prtisne gumb pr XOR vratih da pol predvaja npr: XOR.mp4 na loopu za tko idk x sekund; pol pa se vrne nazaj na prvortn ui),
 # 4. funkcija k bo komunicirala z arduinotom (če se tm zazna da se ksn gumb prtisne da se to poročin in posledično predvaja simulacija)
 def do_smthing(v):
-    if v == 1:
-        print("vrata so bla bla")
-        import time
-        time.sleep(5)
-
+    if type(v) == int:
+        if v == 1:
+            print("vrata so bla bla")
+            import time
+            time.sleep(5)
+    else:pass
 while True:
     if ser.in_waiting > 0:
         message = ser.readline().strip().decode('utf-8')  # read the message from serial
@@ -25,3 +26,6 @@ while True:
         import time
         ct =time.strftime("%H:%M")
         log_rep = f"[at {ct}]\nSerial data: {ser}\nDecoded message: {message}"
+        with open("log.log", "a") as file:
+            file.write(log_rep)
+        do_smthing(message)
