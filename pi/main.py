753b7c848124dc8b5194bf3782ae77e7b524aacd
us3-r <z4nhr1@gmail.com>
2023-03-06 19:22:01 +0100

    sad

--- pi/main.py
+++ pi/main.py
@@ -6,10 +6,11 @@ from util import Start
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 #util.Messages.Terminal.init
-ser.write('Hello from Raspberry Pi!'.encode())
+video_running = False
 
 def get_video(select :str):
     import os
+    global video_running
     gate_list = [
         "and",
         "nand",
@@ -23,19 +24,21 @@ def get_video(select :str):
     #print(path)
     if path is not None: 
         os.system(f"vlc --play-and-exit {path}")
-        print("sleeping...")
         time.sleep(10)
-        print("awake")
+        video_running = False
+
 #Start.init_check()
 
 while True:
-    if ser.in_waiting > 0:
-        message = ser.readline().strip().decode('utf-8')  # read the message from serial
-        #print(message)  # print the message to the console
-        import time
-        ct =time.strftime("%H:%M")
-        log_rep = f"[ at {ct} ]\nSerial data: {ser}\nDecoded message: {message}\n"
-        with open("log.log", "a") as file:
-            file.write(log_rep)
-        get_video(message)
+    if not video_running:
+        if ser.in_waiting > 0:
+            message = ser.readline().strip().decode('utf-8')  # read the message from serial
+            #print(message)  # print the message to the console
+            import time
+            ct =time.strftime("%H:%M")
+            log_rep = f"[ at {ct} ]\nSerial data: {ser}\nDecoded message: {message}\n"
+            with open("log.log", "a") as file:
+                file.write(log_rep)
+            video_running = True
+            get_video(message)
         
