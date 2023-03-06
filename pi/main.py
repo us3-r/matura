43d4b5747fb00aa067638a59315af995692bf572
us3-r <z4nhr1@gmail.com>
2023-03-06 19:35:48 +0100

    s

--- pi/main.py
+++ pi/main.py
@@ -7,6 +7,7 @@ ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 #util.Messages.Terminal.init
 video_running = False
+current_video = ""
 
 def get_video(select :str):
     import os
@@ -34,12 +35,16 @@ while True:
     if not video_running:
         if ser.in_waiting > 0:
             message = ser.readline().strip().decode('utf-8')  # read the message from serial
-            #print(message)  # print the message to the console
+            pass_video = message
             import time
             ct =time.strftime("%H:%M")
             log_rep = f"[ at {ct} ]\nSerial data: {ser}\nDecoded message: {message}\n"
             with open("log.log", "a") as file:
                 file.write(log_rep)
-
-            get_video(message)
+            if pass_video == current_video: pass
+            else:
+                current_video=pass_video
+                get_video(message)
+            #print(message)  # print the message to the console
+            
         
